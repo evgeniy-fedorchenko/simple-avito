5c40a592c23c2949438db027b23062fc57359c8d
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-06-27 00:38:29 +0500

    add dtos

---
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateComment.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class CreateOrUpdateComment {
+
+    @NotBlank
+    @Size(min = 8, max = 64)
+    private String text;
+}
