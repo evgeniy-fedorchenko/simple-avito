679aceda47f31b1d11835baba38391d4bb19f0f3
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-07-06 01:48:09 +0500

    add SimpleAvitoExceptionHandler

---
+++ src/main/java/com/evgeniyfedorchenko/simpleavito/exception/handler/ErrorResponse.java
@@ -0,0 +1,61 @@
+package com.evgeniyfedorchenko.simpleavito.exception.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+import java.util.Objects;
+
+
+/**
+ * Класс, содержащий детали и причины исключения, которое было поймано с помощью {@link SimpleAvitoExceptionHandler}
+ * Создан с целью стандартизировать слишком обобщенный ответ {@code ResponseEntity.badRequest().build()}
+ * Должен быть возвращен в теле ответа при обнаружении ситуаций, требующий возврата {@link HttpStatus#BAD_REQUEST}
+ *
+ * @field code        Пользовательский код ошибки
+ * @field errorsCount Количество найденных нарушений
+ * @field details     Детальное сообщение об обнаруженных нарушениях
+ * @field badObject   Исходный невалидный объект
+ *
+ * @apiNote Пример сериализованого объекта:
+ * <blockquote><pre>
+ * {
+ *   "code": 4,
+ *   "errorsCount": 2,
+ *   "details": {
+ *       "description": "size must be between 8 and 64",
+ *       "title": "size must be between 4 and 32"
+ *   },
+ *   "badObject": {
+ *       "title": "t",
+ *       "price": -1,
+ *       "description": "very vey vey very large text field"
+ *   }
+ * }
+ * </pre></blockquote>
+ */
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public final class ErrorResponse {
+
+    private final int code;
+
+    @JsonProperty("errors_count")
+    private final int errorsCount;
+
+    private final Map<String, String> detailsMap;
+
+    private final String details;
+
+    @JsonProperty("bad_object_name")
+    private final String badObjName;
+
+    @JsonProperty("bad_object_params")
+    private final Object badObj;
+
+}
--- src/main/java/com/evgeniyfedorchenko/simpleavito/exception/handler/SimpleAvitoExceptionHandler.java
+++ src/main/java/com/evgeniyfedorchenko/simpleavito/exception/handler/SimpleAvitoExceptionHandler.java
@@ -2,18 +2,53 @@ package com.evgeniyfedorchenko.simpleavito.exception.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.awt.image.ImagingOpException;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
 public class SimpleAvitoExceptionHandler {
 
+//    Ошибка при обработке переданного изображения
     @ExceptionHandler(ImagingOpException.class)
-    public ResponseEntity<String> handleImagingOpException(ImagingOpException ex) {
+    public ResponseEntity<ErrorResponse> handleImagingOpException(ImagingOpException ex) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(1)
+                .errorsCount(1)
+                .details(ex.getMessage())
+                .build();
         log.trace("ImagingOpException was thrown. Handle and suppress. Ex:", ex);
-        return ResponseEntity.badRequest().body("Cannot processing. Ex:" + ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+//    Проваленная валидация параметров в контроллере
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        Map<String, String> details = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        fieldError -> fieldError.getDefaultMessage() != null
+                                ? fieldError.getDefaultMessage()
+                                : fieldError.getField()));
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(4)
+                .errorsCount(details.size())
+                .detailsMap(details)
+                .badObjName(ex.getBindingResult().getObjectName())
+                .badObj(ex.getBindingResult().getTarget())
+                .build();
+
+        log.trace("MethodArgumentNotValidException was thrown. Handle and suppress. Ex:", ex);
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
+
+
