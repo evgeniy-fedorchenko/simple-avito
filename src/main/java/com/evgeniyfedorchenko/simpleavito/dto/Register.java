802bc2cd71b92f80cf1218fbbde9d1e0634a9a98
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-07-09 23:10:49 +0500

    refactor changelogs for use constraints with sql-tag

--- src/main/java/com/evgeniyfedorchenko/simpleavito/dto/Register.java
+++ src/main/java/com/evgeniyfedorchenko/simpleavito/dto/Register.java
@@ -1,6 +1,7 @@
 package com.evgeniyfedorchenko.simpleavito.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.ToString;
@@ -22,6 +23,7 @@ public class Register {
     @Size(min = 2, max = 16)
     private String lastName;
 
+    @Pattern(regexp = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}")
     private String phone;
 
     private Role role;
